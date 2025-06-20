9026532414d2ead3f888dfd24e9fb571d69fe14a
Büşra Seven <busras3v3n@proton.me>
2025-06-20 13:35:57 +0300

    added function

--- setters.c
+++ setters.c
@@ -1,12 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mutex_functions.c                                  :+:      :+:    :+:   */
+/*   setters.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/20 13:14:22 by busseven          #+#    #+#             */
-/*   Updated: 2025/06/20 13:14:23 by busseven         ###   ########.fr       */
+/*   Created: 2025/06/20 13:30:54 by busseven          #+#    #+#             */
+/*   Updated: 2025/06/20 13:35:31 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philosophers.h"
+
+void	set_int(pthread_mutex_t *mtx, int *i, int set)
+{
+	pthread_mutex_lock(mtx);
+	*i = set;
+	pthread_mutex_unlock(mtx);
+}
+
+void	set_longlong(pthread_mutex_t *mtx, long long *i, long long set)
+{
+	pthread_mutex_lock(mtx);
+	*i = set;
+	pthread_mutex_unlock(mtx);
+}
