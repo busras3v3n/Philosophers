82f448fe1496d705ef30ab27a4414cef128f766c
Büşra Seven <busras3v3n@proton.me>
2025-06-22 18:06:34 +0000

    added read_long

--- setters.c
+++ setters.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   setters.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
+/*   By: busra <busseven@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 13:30:54 by busseven          #+#    #+#             */
-/*   Updated: 2025/06/20 13:35:31 by busseven         ###   ########.fr       */
+/*   Updated: 2025/06/22 18:06:26 by busra            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,14 @@ int	read_int(pthread_mutex_t *mtx, int *i)
 	int	val;
 	pthread_mutex_lock(mtx);
 	val = *i;
+	pthread_mutex_unlock(mtx);
+	return (val);
+}
+long long read_long(pthread_mutex_t *mtx, long long *l)
+{
+	long long val;
+	pthread_mutex_lock(mtx);
+	val = *l;
 	pthread_mutex_unlock(mtx);
 	return (val);
 }
