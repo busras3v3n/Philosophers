b5ba410c2244b6fa987f563a131d3e8a04d6d90d
Büşra Seven <busras3v3n@proton.me>
2025-06-25 14:15:03 +0000

    edited the sleep function

--- routine_utils.c
+++ routine_utils.c
@@ -6,28 +6,29 @@
 /*   By: busra <busseven@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 08:44:29 by busra             #+#    #+#             */
-/*   Updated: 2025/06/24 16:18:15 by busra            ###   ########.fr       */
+/*   Updated: 2025/06/25 14:14:13 by busra            ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "philosophers.h"
 
-void    philo_pause(long long time, int philo_count)
+void    philo_pause(unsigned long long time, int philo_count)
 {
-    int i;
-    int sleep;
+    unsigned long long start_time;
+    unsigned long long target_time;
+    int                 sleep_duration;
 
-    i = 0;
-    sleep = 5;
-    while(i < (time * 1000))
+    start_time = get_current_time();
+    target_time = start_time + time;
+    if(philo_count <= 40)
+        sleep_duration = 10000;
+    else if(philo_count <= 100)
+        sleep_duration = 1000000;
+    else
+        sleep_duration = 500000;
+    while(get_current_time() < target_time)
     {
-        if(philo_count <= 40)
-            sleep = 10;
-        else if(philo_count > 40 && philo_count <= 100)
-            sleep = 100;
-        else
-            sleep = 500;
-        usleep(sleep);
-        i += sleep;
+        if(usleep(sleep_duration))
+            return ;
     }
 }
